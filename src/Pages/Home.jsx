8938ea13d1a5afd0f609f7f9b43e70bb6ff00319
Bipin pandey <142231836+Pandebipin@users.noreply.github.com>
2025-03-13 23:47:43 +0545

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -155,46 +155,73 @@ function Home() {
         </div>
       </div>
 
-      <div className="page3 w-full gap-5 min-h-[100vh] flex justify-center flex-col p-4 bg-white">
-        <ScrollTrigger onEnter={AnimateOn} onExit={AnimateOf}>
-          <div className="images flex flex-col md:flex-row justify-center gap-8 w-full">
-            <img className="img1 w-full md:w-1/2 lg:w-1/3 rounded-lg object-cover" src="https://images.pexels.com/photos/3992949/pexels-photo-3992949.jpeg?cs=srgb&dl=pexels-cdc-library-3992949.jpg&fm=jpg" alt="Classroom" />
-            <img className="img2 w-full md:w-1/2 lg:w-1/3 rounded-lg object-cover" src="https://www.shutterstock.com/image-vector/school-building-bus-front-yard-260nw-322015064.jpg" alt="School Building" />
-          </div>
-          <div className="images flex flex-col pt-4 md:flex-row justify-center gap-7 w-full">
-            <img className="img3 w-full md:w-1/2 lg:w-1/3 rounded-lg object-cover" src="https://img.freepik.com/premium-photo/learning-environment-school-pupils-actively-raising-their-hands-seek-teacher-attention_210545-2574.jpg" alt="Students Raising Hands" />
-            <img className="w-full md:w-1/2 lg:w-1/3 rounded-lg object-cover" src="https://media.istockphoto.com/id/1178221232/photo/diverse-kids-in-a-classroom.jpg?s=612x612&w=0&k=20&c=ClTi4h6MBSRYfJTIwNJWGJPGUIfIlSxH1NeZ15Y_TyA=" alt="Diverse Kids in Classroom" />
-          </div>
-        </ScrollTrigger>
-      </div>
+     <div className="page3 w-full min-h-screen flex flex-col items-center p-4 bg-white">
+  <ScrollTrigger onEnter={AnimateOn} onExit={AnimateOf}>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl">
+      <img className="rounded-lg object-cover w-full aspect-video" 
+        src="https://images.pexels.com/photos/3992949/pexels-photo-3992949.jpeg?cs=srgb&dl=pexels-cdc-library-3992949.jpg&fm=jpg" 
+        alt="Classroom" 
+      />
+      <img className="rounded-lg object-cover w-full aspect-video" 
+        src="https://www.shutterstock.com/image-vector/school-building-bus-front-yard-260nw-322015064.jpg" 
+        alt="School Building" 
+      />
+      <img className="rounded-lg object-cover w-full aspect-video" 
+        src="https://img.freepik.com/premium-photo/learning-environment-school-pupils-actively-raising-their-hands-seek-teacher-attention_210545-2574.jpg" 
+        alt="Students Raising Hands" 
+      />
+      <img className="rounded-lg object-cover w-full aspect-video" 
+        src="https://media.istockphoto.com/id/1178221232/photo/diverse-kids-in-a-classroom.jpg?s=612x612&w=0&k=20&c=ClTi4h6MBSRYfJTIwNJWGJPGUIfIlSxH1NeZ15Y_TyA=" 
+        alt="Diverse Kids in Classroom" 
+      />
+    </div>
+  </ScrollTrigger>
+</div>
 
-      <div className="page4 w-full mt-9 min-h-[60vh] gap-8 p-4 flex justify-center">
-        <div className="flex flex-col-reverse w-full md:flex-row gap-4 justify-between text-center">
-          <div className="left w-full md:w-1/2 flex flex-col justify-center gap-3">
-            <img className="rotate w-20 h-20" src="https://samriddhischool.edu.np/static/media/target.c5c449ad0d30cdde8c8988f9b06de9de.svg" alt="Target" />
-            <h1 className="text-gray-800 flex flex-start pb-2 font-bold text-2xl capitalize">{t("greetings2")}</h1>
-            <p className="text-sm text-gray-600 md:text-md font-serif lg:text-lg">{line2}</p>
-            <p className="text-sm text-gray-600 md:text-md font-serif lg:text-lg">{line3}</p>
-          </div>
-          <div className="right flex flex-start justify-center">
-            <div className="box">{/* images insert */}</div>
-          </div>
-        </div>
-      </div>
 
-      <div className="page5 w-full min-h-[50vh] gap-8 p-4 flex justify-center mt-5">
-        <div className="flex w-full flex-col md:flex-row gap-4 justify-between text-center">
-          <div className="left flex justify-center">
-            <div className="box2">{/* images insert */}</div>
-          </div>
-          <div className="left w-full md:w-1/2 flex flex-col gap-3 p-4">
-            <img className="rotate w-20 h-20" src="https://samriddhischool.edu.np/static/media/vision.713bcd62959838335dc58f2f73848028.svg" alt="Vision" />
-            <h1 className="text-gray-800 flex flex-start pb-2 font-bold text-2xl capitalize">{t("greetings3")}</h1>
-            <p className="text-sm tracking-wide text-gray-600 md:text-md font-serif lg:text-lg">{line4}</p>
-            <p className="text-sm tracking-wide text-gray-600 md:text-md font-serif lg:text-lg">{line5}</p>
-          </div>
-        </div>
-      </div>
+      <div className="page4 w-full mt-9 min-h-[60vh] p-6 flex justify-center bg-white">
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl items-center text-center md:text-left">
+    {/* Left Section */}
+    <div className="flex flex-col items-center md:items-start gap-4">
+      <img 
+        className="rotate max-w-[80px] sm:max-w-[100px]" 
+        src="https://samriddhischool.edu.np/static/media/target.c5c449ad0d30cdde8c8988f9b06de9de.svg" 
+        alt="Target" 
+      />
+      <h1 className="text-gray-800 font-bold text-2xl sm:text-3xl capitalize">{t("greetings2")}</h1>
+      <p className="text-gray-600 text-base sm:text-lg lg:text-xl font-serif">{line2}</p>
+      <p className="text-gray-600 text-base sm:text-lg lg:text-xl font-serif">{line3}</p>
+    </div>
+
+    {/* Right Section */}
+    <div className="flex justify-center md:justify-start">
+      <div className="box">{/* Insert Images Here */}</div>
+    </div>
+  </div>
+</div>
+
+
+   <div className="page5 w-full min-h-[50vh] p-6 flex justify-center mt-5 bg-white">
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl items-center text-center md:text-left">
+    {/* Left Section */}
+    <div className="flex justify-center">
+      <div className="box2">{/* Insert Images Here */}</div>
+    </div>
+
+    {/* Right Section */}
+    <div className="flex flex-col items-center md:items-start gap-4 p-4">
+      <img 
+        className="rotate max-w-[80px] sm:max-w-[100px]" 
+        src="https://samriddhischool.edu.np/static/media/vision.713bcd62959838335dc58f2f73848028.svg" 
+        alt="Vision" 
+      />
+      <h1 className="text-gray-800 font-bold text-2xl sm:text-3xl capitalize">{t("greetings3")}</h1>
+      <p className="text-gray-600 text-base sm:text-lg lg:text-xl font-serif tracking-wide">{line4}</p>
+      <p className="text-gray-600 text-base sm:text-lg lg:text-xl font-serif tracking-wide">{line5}</p>
+    </div>
+  </div>
+</div>
+
 
       <div className="page4 w-full mt-8 min-h-[70vh] gap-8 p-4 flex justify-center">
         <div className="flex flex-col-reverse w-full md:flex-row gap-4 justify-between text-center">
