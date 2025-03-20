0902f07dbfea97993fc0fa2068d00bfc4b545177
Bipin pandey <142231836+Pandebipin@users.noreply.github.com>
2025-03-20 17:01:07 +0545

    Update Allteacherbio.jsx

--- src/Components/Allteacherbio.jsx
+++ src/Components/Allteacherbio.jsx
@@ -29,7 +29,7 @@ const TeacherBio = () => {
         )}
         
       
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-6 md:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-7 md:gap-6">
           {teachers.map((teacher) => (
             <div
               key={teacher.id}
