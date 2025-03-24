7a0b2f6ec38e8237ddaf59f543ff263912dd1d19
Bipin pandey <142231836+Pandebipin@users.noreply.github.com>
2025-03-24 18:48:37 +0545

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -278,10 +278,10 @@ function Home() {
         <Testomonial />
       </div>
 
-      <div className="page-8 w-full min-h-[60vh] gap-7 bg-white flex flex-col justify-center">
+      <div className="page-8 w-full min-h-[60vh] gap-7 bg-white mt-6 md:mt-1 flex flex-col justify-center">
         <div className="flex justify-between p-4 w-full">
-          <h1 className="text-blue-800 font-sans font-bold p-2 capitalize text-2xl border-b-2 border-blue-500">Recent Blogs</h1>
-          <h1 className="text-gray-800 text-xl cursor-pointer" onClick={() => navigate("/blogposts")}>View More</h1>
+          <h1 className="text-blue-800 font-sans font-bold p-2 capitalize text-lg md:text-2xl border-b-2 border-blue-500">Recent Blogs</h1>
+          <h1 className="text-gray-800 md:text-xl text-md cursor-pointer" onClick={() => navigate("/blogposts")}>View More</h1>
         </div>
         <div className="grid p-3 grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {Blogs.slice(0, 4).map((blog) => (
